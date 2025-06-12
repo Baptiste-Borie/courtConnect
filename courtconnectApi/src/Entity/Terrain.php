4439d2f1d8a87b9467db8e319b24823d543dae58
Baptiste Borie <baptiste.borie@sports-decisions.com>
2025-06-12 16:28:57 +0200

    Init v2

---
+++ courtconnectApi/src/Entity/Terrain.php
@@ -0,0 +1,363 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TerrainRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: TerrainRepository::class)]
+class Terrain
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    #[Groups(['terrain'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['terrain'])]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['terrain'])]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['terrain'])]
+    private ?string $ville = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['terrain'])]
+    private ?string $code_postal = null;
+
+    #[ORM\Column]
+    #[Groups(['terrain'])]
+    private ?float $latitude = null;
+
+    #[ORM\Column(type: 'float')]
+    #[Groups(['terrain'])]
+    private ?float $longitude = null;
+
+    #[ORM\Column(type: 'datetime')]
+    #[Groups(['terrain'])]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['terrain'])]
+    private ?string $sol = null;
+
+    #[ORM\Column(type: 'float')]
+    #[Groups(['terrain'])]
+    private ?int $nb_panier = null;
+
+    #[ORM\Column(type: 'float')]
+    #[Groups(['terrain'])]
+    private ?string $type_filet = null;
+
+    #[ORM\Column(type: 'boolean')]
+    #[Groups(['terrain'])]
+    private ?bool $spectateur = null;
+
+    #[ORM\ManyToOne(inversedBy: 'terrains')]
+    #[Groups(['terrain'])]
+    private ?User $created_by = null;
+
+    /**
+     * @var Collection<int, Event>
+     */
+    #[ORM\OneToMany(targetEntity: Event::class, mappedBy: 'terrain')]
+    private Collection $events;
+
+    /**
+     * @var Collection<int, User>
+     */
+    #[ORM\ManyToMany(targetEntity: User::class, inversedBy: 'favori')]
+    private Collection $favori;
+
+    #[ORM\Column(length: 255)]
+    private ?string $etat = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $remarque = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type_panier = null;
+
+    #[ORM\Column(type: 'integer')]
+    private ?int $usure = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image_url = null;
+
+    public function __construct()
+    {
+        $this->events = new ArrayCollection();
+        $this->favori = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): static
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->code_postal;
+    }
+
+    public function setCodePostal(string $code_postal): static
+    {
+        $this->code_postal = $code_postal;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): static
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): static
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getSol(): ?string
+    {
+        return $this->sol;
+    }
+
+    public function setSol(string $sol): static
+    {
+        $this->sol = $sol;
+
+        return $this;
+    }
+
+    public function getNbPanier(): ?int
+    {
+        return $this->nb_panier;
+    }
+
+    public function setNbPanier(int $nb_panier): static
+    {
+        $this->nb_panier = $nb_panier;
+
+        return $this;
+    }
+
+    public function getTypeFilet(): ?bool
+    {
+        return $this->type_filet;
+    }
+
+    public function setTypeFilet(string $filet): static
+    {
+        $this->type_filet = $filet;
+
+        return $this;
+    }
+
+    public function isSpectateur(): ?bool
+    {
+        return $this->spectateur;
+    }
+
+    public function setSpectateur(bool $spectateur): static
+    {
+        $this->spectateur = $spectateur;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?User
+    {
+        return $this->created_by;
+    }
+
+    public function setCreatedBy(?User $created_by): static
+    {
+        $this->created_by = $created_by;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Event>
+     */
+    public function getEvents(): Collection
+    {
+        return $this->events;
+    }
+
+    public function addEvent(Event $event): static
+    {
+        if (!$this->events->contains($event)) {
+            $this->events->add($event);
+            $event->setTerrain($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvent(Event $event): static
+    {
+        if ($this->events->removeElement($event)) {
+            // set the owning side to null (unless already changed)
+            if ($event->getTerrain() === $this) {
+                $event->setTerrain(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getFavori(): Collection
+    {
+        return $this->favori;
+    }
+
+    public function addFavori(User $favori): static
+    {
+        if (!$this->favori->contains($favori)) {
+            $this->favori->add($favori);
+        }
+
+        return $this;
+    }
+
+    public function removeFavori(User $favori): static
+    {
+        $this->favori->removeElement($favori);
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): static
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getRemarque(): ?string
+    {
+        return $this->remarque;
+    }
+
+    public function setRemarque(string $remarque): static
+    {
+        $this->remarque = $remarque;
+
+        return $this;
+    }
+
+    public function getTypePanier(): ?string
+    {
+        return $this->type_panier;
+    }
+
+    public function setTypePanier(string $type_panier): static
+    {
+        $this->type_panier = $type_panier;
+
+        return $this;
+    }
+
+    public function getUsure(): ?int
+    {
+        return $this->usure;
+    }
+
+    public function setUsure(int $usure): static
+    {
+        $this->usure = $usure;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->image_url;
+    }
+
+    public function setImageUrl(string $image): static
+    {
+        $this->image_url = $image;
+
+        return $this;
+    }
+}
---
+++ courtconnectApi/src/Entity/User.php
@@ -0,0 +1,303 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['username'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['terrain'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    #[Groups(['terrain'])]
+    private ?string $username = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    #[Groups(['terrain'])]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['terrain'])]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['terrain'])]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['terrain'])]
+    private ?string $pseudo = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['terrain'])]
+    private ?string $image_url = null;
+
+    /**
+     * @var Collection<int, Terrain>
+     */
+    #[ORM\OneToMany(targetEntity: Terrain::class, mappedBy: 'created_by')]
+    private Collection $terrains;
+
+    /**
+     * @var Collection<int, Event>
+     */
+    #[ORM\OneToMany(targetEntity: Event::class, mappedBy: 'created_by')]
+    private Collection $events;
+
+    /**
+     * @var Collection<int, Terrain>
+     */
+    #[ORM\ManyToMany(targetEntity: Terrain::class, mappedBy: 'favori')]
+    private Collection $favori;
+
+    #[ORM\Column]
+    private ?int $trustability = null;
+
+    public function __construct()
+    {
+        $this->terrains = new ArrayCollection();
+        $this->events = new ArrayCollection();
+        $this->favori = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(?string $pseudo): static
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getPhotoProfil(): ?string
+    {
+        return $this->photo_profil;
+    }
+
+    public function setPhotoProfil(?string $photo_profil): static
+    {
+        $this->photo_profil = $photo_profil;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Terrain>
+     */
+    public function getTerrains(): Collection
+    {
+        return $this->terrains;
+    }
+
+    public function addTerrain(Terrain $terrain): static
+    {
+        if (!$this->terrains->contains($terrain)) {
+            $this->terrains->add($terrain);
+            $terrain->setCreatedBy($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTerrain(Terrain $terrain): static
+    {
+        if ($this->terrains->removeElement($terrain)) {
+            // set the owning side to null (unless already changed)
+            if ($terrain->getCreatedBy() === $this) {
+                $terrain->setCreatedBy(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Event>
+     */
+    public function getEvents(): Collection
+    {
+        return $this->events;
+    }
+
+    public function addEvent(Event $event): static
+    {
+        if (!$this->events->contains($event)) {
+            $this->events->add($event);
+            $event->setCreatedBy($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvent(Event $event): static
+    {
+        if ($this->events->removeElement($event)) {
+            // set the owning side to null (unless already changed)
+            if ($event->getCreatedBy() === $this) {
+                $event->setCreatedBy(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Terrain>
+     */
+    public function getFavori(): Collection
+    {
+        return $this->favori;
+    }
+
+    public function addFavori(Terrain $favori): static
+    {
+        if (!$this->favori->contains($favori)) {
+            $this->favori->add($favori);
+            $favori->addFavori($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFavori(Terrain $favori): static
+    {
+        if ($this->favori->removeElement($favori)) {
+            $favori->removeFavori($this);
+        }
+
+        return $this;
+    }
+
+    public function getTrustability(): ?int
+    {
+        return $this->trustability;
+    }
+
+    public function setTrustability(int $trustability): static
+    {
+        $this->trustability = $trustability;
+
+        return $this;
+    }
+}
