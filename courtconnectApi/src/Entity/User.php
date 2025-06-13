79377b2e580759dc94eff1dd9dc16f005d71ff7d
JoshuaChongToua <chongtouajoshua@gmail.com>
2025-06-13 13:32:08 +0200

    register + update

--- courtconnectApi/src/Entity/User.php
+++ courtconnectApi/src/Entity/User.php
@@ -21,7 +21,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
-    #[Groups(['terrain'])]
+    #[Groups(['terrain', 'user'])]
     private ?string $username = null;
 
     /**
