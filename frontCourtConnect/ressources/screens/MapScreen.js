840668170eb56d0b957080a7b76a302c41a95dd7
Nassjet <nass.belkacem7@gmail.com>
2025-06-13 16:50:41 +0200

    add ReturnButton

--- frontCourtConnect/ressources/screens/MapScreen.js
+++ frontCourtConnect/ressources/screens/MapScreen.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Button } from "react-native";
 import MapBox from "../shared/MapBox";
+import ReturnButton from "../shared/ReturnButton";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const MapScreen = () => {
+const MapScreen = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
-      <MapBox height="100%" />
+      <MapBox height="100%" /> 
+      <ReturnButton  onPress={() => {navigation.navigate("Home")}} />
     </SafeAreaView>
   );
 };
+//ReturnButton à enlever quand le layout sera pret
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor:"black",
   },
 });
 
---
+++ frontCourtConnect/ressources/shared/ReturnButton.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const ReturnButton = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>← RETOUR</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "transparant",
+    paddingVertical: 25,
+    borderRadius: 25,
+    position: "absolute",
+    top: 50,
+    left: 20,
+  },
+  text: {
+    color: "#232543",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
+
+
+export default ReturnButton;
