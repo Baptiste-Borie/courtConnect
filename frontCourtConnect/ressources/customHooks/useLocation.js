70141076b9b3207112b20f2501be544006cced63
Baptiste Borie <baptiste.borie@sports-decisions.com>
2025-06-16 13:46:30 +0200

    06_16 clean console.log

--- frontCourtConnect/ressources/customHooks/useLocation.js
+++ frontCourtConnect/ressources/customHooks/useLocation.js
@@ -1,39 +1,38 @@
-import { View, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import * as Location from 'expo-location'
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 export default function useLocation() {
-    const [errorMsg, setErrorMsg] = useState("");
-    const [longitude, setLongitude] = useState("");
-    const [latitude, setLatitude] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [latitude, setLatitude] = useState("");
 
-    const getUserLocation = async () => {
-        let {status} = await Location.requestForegroundPermissionsAsync(); 
+  const getUserLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
 
-        if (status !== 'granted') {
-            setErrorMsg("La permission d'accès à la localisation n'a pas été accordé");
-            return;
-        }
-
-        let {coords} = await Location.getCurrentPositionAsync();
-
-        if (coords) {
-            const {latitude, longitude} = coords;
-            console.log('lat and long are', latitude, longitude); 
-            setLatitude(latitude); 
-            setLongitude(longitude); 
-            let response = await Location.reverseGeocodeAsync({
-                latitude,
-                longitude
-            })
-
-            console.log("USER LOCATION IS", response);
-        }
+    if (status !== "granted") {
+      setErrorMsg(
+        "La permission d'accès à la localisation n'a pas été accordé"
+      );
+      return;
     }
 
-    useEffect(() => {
-        getUserLocation();
-    }, [])
+    let { coords } = await Location.getCurrentPositionAsync();
 
-    return {latitude,longitude, errorMsg}
+    if (coords) {
+      const { latitude, longitude } = coords;
+      setLatitude(latitude);
+      setLongitude(longitude);
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+    }
+  };
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  return { latitude, longitude, errorMsg };
 }
