3d24f81592baa9afda1b42c6b1d47588b9b8de7f
JoshuaChongToua <chongtouajoshua@gmail.com>
2025-06-16 10:25:39 +0200

    getTerrain/id + userConnected

--- courtconnectApi/src/Controller/UserController.php
+++ courtconnectApi/src/Controller/UserController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Dto\UserDTO;
 use App\Entity\User;
 use App\Manager\UserManager;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -13,23 +15,46 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserManager $userManager, UserPasswordHasherInterface $passwordHasher): Response
+    public function __construct(private UserManager $userManager, private UserPasswordHasherInterface $passwordHasher, private UserRepository $userRepository)
     {
-        $user = new User();
+
+    }
+
+    #[Route('/api/userConnected', name: 'app_user_connected', methods: ['GET'])]
+    public function userConnected(Request $request): JsonResponse
+    {
+        return $this->json($this->getUser(), 200, [], ['groups' => ['user']]);
+
+    }
+
+    #[Route('/register', name: 'app_register', methods: ['POST'])]
+    public function register(Request $request): JsonResponse
+    {
         $data = json_decode($request->getContent(), true);
+
+        if (!$data || empty($data['username']) || empty($data['password'])) {
+            return $this->json(['message' => 'Données invalides'], 400);
+        }
+
+        $existingUser = $this->userRepository->findOneBy(['username' => $data['username']]);
+        if ($existingUser) {
+            return $this->json(['message' => 'Adresse e-mail déjà utilisée.'], 400);
+        }
+
+        $user = new User();
         $dto = new UserDTO();
         $dto->username = $data['username'];
-        $dto->password = $passwordHasher->hashPassword($user, $data['password']);
+        $dto->password = $this->passwordHasher->hashPassword($user, $data['password']);
 
-        $user = $userManager->addUser($dto);
+        $user = $this->userManager->addUser($dto);
 
         if (!$user) {
-            return $this->json(['message' => 'Erreur lors de la création du user.'], 500);
+            return $this->json(['message' => 'Erreur lors de la création du compte.'], 500);
         }
 
         return $this->json($user, 200, [], ['groups' => ['user']]);
     }
+
 
     #[Route('/api/updateUser', name: 'app_update_user')]
     public function updateUser(Request $request, UserManager $userManager, UserPasswordHasherInterface $passwordHasher): Response
