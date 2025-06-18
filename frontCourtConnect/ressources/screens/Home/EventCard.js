08662795e2b5838ca8e815de59ff917cb4d49b47
Baptiste Borie <baptiste.borie@sports-decisions.com>
2025-06-18 14:49:09 +0200

    06_18 join event

--- frontCourtConnect/ressources/screens/Home/EventCard.js
+++ frontCourtConnect/ressources/screens/Home/EventCard.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-export default function EventCard({ event, distance, onPress, theme }) {
+export default function EventCard({ event, distance, theme }) {
+  const navigation = useNavigation();
+  const handlePress = () => {
+    navigation.navigate("EventDetail", { eventId: event.id });
+  };
   return (
-    <TouchableOpacity style={[styles.card]} onPress={onPress}>
+    <TouchableOpacity style={[styles.card]} onPress={handlePress}>
       <View
         style={[
           styles.imagePlaceholder,
           { backgroundColor: theme.background_light },
         ]}
       />
-      <Text style={[styles.name, { color: theme.text }]}>
-        {event.terrain.nom}
-      </Text>
+      <Text style={[styles.name, { color: theme.text }]}>{event.nom}</Text>
       <Text style={{ color: theme.primary, fontSize: 12 }}>
         {distance.toFixed(1)} km
       </Text>
